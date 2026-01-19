f371fb4b4289cf8ee4ccdedd7ccf629acc248637
shivenupadhyay8-art <shivenupadhyay8@gmail.com>
2026-01-19 23:58:33 +0530

    Add files via upload

---
+++ 07reactrouter/src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout.jsx'
+import Home from './components/home/home.jsx'
+import About from './components/about/about.jsx'
+
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children: [
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router}/>
+  </StrictMode>,
+)
